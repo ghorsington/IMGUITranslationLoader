3e09b748cd596fdb35340485b79dc742e02e21c0
denikson <geoffrey.hoooooorse@gmail.com>
2018-11-20 11:59:07 +0200

    Fix not working on older Unity; remove ReiPatcher; bump version

--- IMGUITranslationLoader.Sybaris.Patcher/Patcher.cs
+++ IMGUITranslationLoader.Sybaris.Patcher/Patcher.cs
@@ -1,10 +1,10 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Reflection;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using Mono.Cecil.Inject;
+using MethodBody = Mono.Cecil.Cil.MethodBody;
 
 namespace IMGUITranslationLoader.Sybaris.Patcher
 {
@@ -12,6 +12,28 @@ namespace IMGUITranslationLoader.Sybaris.Patcher
     {
         public static readonly string[] TargetAssemblyNames = {"UnityEngine.dll"};
         private const string HOOK_NAME = "IMGUITranslationLoader.Hook";
+
+        public static void Patch(AssemblyDefinition assembly)
+        {
+            string assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string hookDir = $"{HOOK_NAME}.dll";
+            AssemblyDefinition hookAssembly = AssemblyLoader.LoadAssembly(Path.Combine(assemblyDir, hookDir));
+
+            TypeDefinition guiContent = assembly.MainModule.GetType("UnityEngine.GUIContent");
+
+            TypeDefinition hook = hookAssembly.MainModule.GetType($"{HOOK_NAME}.TranslationHooks");
+
+            MethodDefinition onTranslateContentText = hook.GetMethod("OnTranslateGuiContent");
+
+            PatchGui(assembly.MainModule.GetType("UnityEngine.GUI"), hook);
+            PatchGui(assembly.MainModule.GetType("UnityEngine.GUILayout"), hook);
+            PatchGUIContent(guiContent, hook);
+
+            guiContent.GetMethod("set_text").InjectWith(onTranslateContentText, flags: InjectFlags.PassParametersRef);
+
+            guiContent.GetMethod("set_tooltip")
+                      .InjectWith(onTranslateContentText, flags: InjectFlags.PassParametersRef);
+        }
 
         private static void PatchGui(TypeDefinition gui, TypeDefinition hook)
         {
@@ -42,51 +64,41 @@ namespace IMGUITranslationLoader.Sybaris.Patcher
             }
         }
 
-        public static void Patch(AssemblyDefinition assembly)
+        private static void PatchGUIContent(TypeDefinition guiContent, TypeDefinition hook)
         {
-            string assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string hookDir = $"{HOOK_NAME}.dll";
-            AssemblyDefinition hookAssembly = AssemblyLoader.LoadAssembly(Path.Combine(assemblyDir, hookDir));
-
-            TypeDefinition guiContent = assembly.MainModule.GetType("UnityEngine.GUIContent");
-
-            TypeDefinition hook = hookAssembly.MainModule.GetType($"{HOOK_NAME}.TranslationHooks");
-
-            FieldDefinition insideCtorField = hook.GetField("InsideContentCtor");
             MethodDefinition onTranslateTextTooltip = hook.GetMethod("OnTranslateTextTooltip");
-            MethodDefinition onTranslateContentText = hook.GetMethod("OnTranslateGuiContent");
-
-            PatchGui(assembly.MainModule.GetType("UnityEngine.GUI"), hook);
-            PatchGui(assembly.MainModule.GetType("UnityEngine.GUILayout"), hook);
-
             FieldDefinition mText = guiContent.GetField("m_Text");
             FieldDefinition mTooltip = guiContent.GetField("m_Tooltip");
+            FieldDefinition insideCtorField = hook.GetField("InsideContentCtor");
 
-            MethodDefinition ctorString =
-                    guiContent.GetMethod(".ctor", "System.String", "UnityEngine.Texture", "System.String");
-            ILProcessor il = ctorString.Body.GetILProcessor();
-            Instruction firstIns = ctorString.Body.Instructions.First();
-            il.InsertBefore(firstIns, il.Create(OpCodes.Ldc_I4_1));
-            il.InsertBefore(firstIns, il.Create(OpCodes.Stsfld, assembly.MainModule.ImportReference(insideCtorField)));
-            ctorString.InjectWith(onTranslateTextTooltip,
-                                  -1,
-                                  flags: InjectFlags.PassFields,
-                                  typeFields: new[] {mText, mTooltip});
+            foreach (MethodDefinition ctor in guiContent.Methods.Where(m => m.IsConstructor
+                                                                            && !m.IsStatic
+                                                                            && m.Parameters.Count > 0))
+            {
+                if (!ctor.HasBody)
+                    continue;
+                MethodBody body = ctor.Body;
 
-            MethodDefinition ctorObj = guiContent.GetMethod(".ctor", "UnityEngine.GUIContent");
-            il = ctorObj.Body.GetILProcessor();
-            firstIns = ctorObj.Body.Instructions.First();
-            il.InsertBefore(firstIns, il.Create(OpCodes.Ldc_I4_1));
-            il.InsertBefore(firstIns, il.Create(OpCodes.Stsfld, assembly.MainModule.ImportReference(insideCtorField)));
-            ctorObj.InjectWith(onTranslateTextTooltip,
-                               -1,
-                               flags: InjectFlags.PassFields,
-                               typeFields: new[] {mText, mTooltip});
+                if (ctor.Parameters.All(p => p.ParameterType.FullName != "System.String"
+                                             && p.ParameterType.FullName != "UnityEngine.GUIContent"))
+                    continue;
 
-            guiContent.GetMethod("set_text").InjectWith(onTranslateContentText, flags: InjectFlags.PassParametersRef);
+                if (body.Instructions.Any(ins => ins.OpCode == OpCodes.Call
+                                                 && ((MethodReference) ins.Operand).Name == ".ctor"
+                                                 && ((MethodReference) ins.Operand).DeclaringType.FullName
+                                                 == guiContent.FullName))
+                    continue;
 
-            guiContent.GetMethod("set_tooltip")
-                      .InjectWith(onTranslateContentText, flags: InjectFlags.PassParametersRef);
+                ILProcessor il = ctor.Body.GetILProcessor();
+                Instruction firstIns = ctor.Body.Instructions.First();
+                il.InsertBefore(firstIns, il.Create(OpCodes.Ldc_I4_1));
+                il.InsertBefore(firstIns,
+                                il.Create(OpCodes.Stsfld, guiContent.Module.ImportReference(insideCtorField)));
+                ctor.InjectWith(onTranslateTextTooltip,
+                                -1,
+                                flags: InjectFlags.PassFields,
+                                typeFields: new[] {mText, mTooltip});
+            }
         }
     }
 }
